953d3659fd65495ef80ddce905eeb839d54b8b75
Ephraim Neil Junsan <Ephraim Neil Junsan>
2016-03-17 15:35:02 +0800

    done

---
+++ assets/js/scrolltop-fixed.js
@@ -0,0 +1,11 @@
+$(window).bind('scroll', function () {
+	 
+	   if ($(window).scrollTop() > 0) {
+	    
+	 $('.navbar').addClass('fixed');	    
+	} 
+	else
+	{	        
+	$('.navbar').removeClass('fixed');	   
+	}
+	});
